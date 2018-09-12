847f691ddd795f43f18610641a8d4b171f4554d8
zionmaori <zion.maori@gmail.com>
2018-09-12 18:15:21 +0300

    12-09-18 train

--- lesson2/exc2b.py
+++ lesson2/exc2b.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-var1 = (raw_input("enter first string : "))
-var2 = (raw_input("enter second string : "))
+var1 = (input("enter first string : "))
+var2 = (input("enter second string : "))
 var3 = (int(input("enter a number between 1 and 3 : ")))
 if (var3 < 1) or (var3 > 3) :
 	var3 = (int(input("please try again : ")))	
@@ -11,10 +11,21 @@ var2len = len(var2)
 
 if (var1len > var2len) and (var1len > var3) :
 	print("var1 length is bigger ")
+	biggerVar = var1
 elif (var2len > var1len) and (var2len > var3) :
 	print("var2 length is bigger ") 
+	biggerVar = var2
 elif (var3 > var1len) and (var3 > var2len) :
 	print("var3 length is bigger ")
-
-if (var1 in var2 and var3) :
-	print var1 ("is in them")
+	return biggerVar == var3
+#b
+if (var1 in var2 or var3) :
+	print  (var1," is in them")
+elif (var2 in var1 or var3) :
+	print (var2, " is in them")
+elif (var3 in var2 or var1) :
+	print  (var3, " is in them")
+	
+#c
+posone = biggerVar[0:var3]
+print(str(posone))
